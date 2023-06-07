2da0f7476b364c3f1de18f0cc309d339cfe8a297
Hamsterrific <simbul@gmail.com>
2023-06-07 17:51:23 +0400

    fix makefile

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,6 @@ const config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
       },
       { test: /\.css$/, use: ["style-loader", "css-loader", "postcss-loader"] },
       {
